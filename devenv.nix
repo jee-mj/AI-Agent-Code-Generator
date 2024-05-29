c81b79dfb58e232b77890c901d0c9d2881cdcbe0
jee-mj <28581723+jee-mj@users.noreply.github.com>
2024-05-30 03:45:00 +1000

    init for troubleshooting

---
+++ devenv.nix
@@ -0,0 +1,53 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [pkgs.git];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+  languages.c.enable = true;
+
+  languages.python = {
+    enable = true;
+    #manylinux.enable = false;
+    poetry.enable = true;
+    package = pkgs.python3Full; # Use the stable Python package from nixpkgs
+    venv = {
+      enable = true;
+      quiet = true;
+    };
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
